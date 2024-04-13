2a971d3a29c7085535d8d0045550f11bdee629ec
FadyMilad22 <elfadyy2010@yahoo.com>
2024-04-13 13:01:15 +0200

    Organising App Architecture for the Auth activity

--- app/src/main/java/com/example/e_commerceapp/Authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/example/e_commerceapp/Authentication/AuthenticationActivity.kt
@@ -1,9 +1,10 @@
-package com.example.e_commerceapp
+package com.example.e_commerceapp.Authentication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.e_commerceapp.R
 
-class MainActivity : AppCompatActivity() {
+class AuthenticationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
